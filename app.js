9a5e57fb7ab951e6060ea710ed9a4c5ffbe3fb37
YWKlai <ywklai666@gmail.com>
2024-07-09 11:38:14 +0800

    卸載和重新安裝 express-handlebars

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const { engine } = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const app = express();
 
 const restaurants = require('./restaurant.json');
 
-app.engine('handlebars', engine({ defaultLayout: 'main' }));
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 app.use(bodyParser.urlencoded({ extended: true }));
